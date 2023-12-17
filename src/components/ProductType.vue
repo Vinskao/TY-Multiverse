1444d900d05c97258109b6432f49471243f65476
Vinskao <tianyikao@gmail.com>
2023-12-17 22:59:43 +0800

    send to 8080

--- src/components/ProductType.vue
+++ src/components/ProductType.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <!-- Your template content here -->
+    <button @click="fetchData">Fetch Data</button>
+    <div v-if="errors && Object.keys(errors).length > 0">
+      <h2>Error Messages:</h2>
+      <ul>
+        <li v-for="(message, field) in errors" :key="field">
+          {{ field }}: {{ message }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 
-const param = ref({});
 const errors = ref({});
-const deleteData = ref(undefined);
-const insertData = ref(undefined);
-const updateData = ref(undefined);
-const prodaction = ref("");
 
 const clearForm = () => {
-  // 清空表单逻辑
+  // 清空表單逻辑
   var inputs = document.getElementsByTagName("input");
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].type == "text") {
@@ -24,36 +27,35 @@ const clearForm = () => {
   }
 };
 
-const fetchData = () => {
-  // 使用 Fetch API 发送请求获取数据
-  fetch("http://192.168.74.54:8080/pages/ajax/products", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      // 如果有需要，您可以在这里添加要发送到后端的数据
-    }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.success);
-      if (data.success === false) {
-        console.log(data);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+const fetchData = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/pages/ajax/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // 如果有需要，您可以在这里添加要发送到后端的数据
+      }),
     });
+
+    const data = await response.json();
+
+    if (data.errors) {
+      // 如果後端返回錯誤，處理錯誤消息
+      errors.value = data.errors;
+    } else {
+      // 如果成功，處理返回的數據
+      console.log(data);
+      // 在這裡處理接收到的數據，例如更新本地數據狀態
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
 
 onMounted(() => {
   // 這裡的程式碼在客戶端渲染後執行
   clearForm();
 });
-
-export default {
-  clearForm,
-  fetchData,
-};
 </script>
