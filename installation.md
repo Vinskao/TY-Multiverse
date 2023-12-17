9acf4b442943bb5a6c7c6283255f4112ead5685c
Vinskao <tianyikao@gmail.com>
2023-12-17 17:14:41 +0800

    reversal

--- installation.md
+++ installation.md
@@ -3,6 +3,8 @@ npm install dotenv
 npm install axios
 npm install astro
 npm i bootstrap@5.3.2
+npx astro add vue
+npm install vue @vue/cli-plugin-typescript --save-dev
 
 npm install express
 npm i --save-dev @types/jquery
--- src/components/ProductType.vue
+++ src/components/ProductType.vue
@@ -1,56 +1,59 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <!-- Your template content here -->
+  </div>
 </template>
 
-<script src="./script.js">
-import "vue@3";
-const { createApp } = Vue;
+<script setup>
+import { ref, onMounted } from "vue";
 
-const app = createApp({
-  data() {
-    return {
-      param: {},
-      errors: {},
-      deleteData: undefined,
-      insertData: undefined,
-      updateData: undefined,
-      prodaction: "",
-    };
-  },
-  methods: {
-    clearForm() {
-      // 清空表单逻辑
-      var inputs = document.getElementsByTagName("input");
-      for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == "text") {
-          inputs[i].value = "";
-        }
+const param = ref({});
+const errors = ref({});
+const deleteData = ref(undefined);
+const insertData = ref(undefined);
+const updateData = ref(undefined);
+const prodaction = ref("");
+
+const clearForm = () => {
+  // 清空表单逻辑
+  var inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == "text") {
+      inputs[i].value = "";
+    }
+  }
+};
+
+const fetchData = () => {
+  // 使用 Fetch API 发送请求获取数据
+  fetch("http://192.168.74.54:8080/pages/ajax/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // 如果有需要，您可以在这里添加要发送到后端的数据
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.success);
+      if (data.success === false) {
+        console.log(data);
       }
-    },
-    fetchData() {
-      // 使用 Fetch API 发送请求获取数据
-      fetch("http://192.168.74.54:8080/pages/ajax/products", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          // 如果有需要，您可以在这里添加要发送到后端的数据
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.success);
-          if (data.success === false) {
-            console.log(data);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    },
-  },
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+onMounted(() => {
+  // 這裡的程式碼在客戶端渲染後執行
+  clearForm();
 });
 
-app.mount("#app");
+export default {
+  clearForm,
+  fetchData,
+};
 </script>
